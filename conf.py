3128a806151267ed589bbbabd1710edc99b9d468
Ward Weistra <ward@fire.ly>
2023-08-02 14:23:22 +0200

    Fixing errors on local build

--- conf.py
+++ conf.py
@@ -65,7 +65,7 @@ release = u'3.5.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -291,5 +291,5 @@ intersphinx_mapping = {
     'forge_docs': ('https://docs.fire.ly/projects/Forge/', None),
     'firely_net_sdk_docs': ('https://docs.fire.ly/projects/Firely-NET-SDK/', None),
     'simplifier_docs': ('https://docs.fire.ly/projects/Simplifier/', None),
-    'fhir_mapper_docs': ('https://docs.fire.ly/projects/FHIR-Mapper/', None)
+    'firely_server_docs': ('https://docs.fire.ly/projects/Firely-Server/en/latest/', None)
 }
